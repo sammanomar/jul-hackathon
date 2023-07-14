e769381eaff8946052521b0fd6da8302ddf20802
SamMartin92 <samm92@live.ie>
2023-07-14 21:06:13 +0000

    add object of surfspots

--- script.js
+++ script.js
@@ -7,17 +7,81 @@ let pos = await returnCoordinates();
 
 //Surf spots array
 const surfSpots = [
-  { lat: 48.39276, lng: 135.72473 },
-  { lat: 50.087, lng: 14.421 },
-  { lat: 52.75672962610946, lng: -6.245176598717489 },
-  { lat: 52.18789747091325, lng: -6.547300610706609 },
-  { lat: 51.99890520306989, lng: -7.623960726158749 },
+  {
+    name: "Fistral Beach, Cornwall, England",
+    location: { lat: 50.418472, lng: -5.098361 },
+  },
+  {
+    name: "Perranporth, Cornwall, England",
+    location: { lat: 50.351417, lng: -5.156333 },
+  },
+  {
+    name: "Porthleven, Cornwall, England",
+    location: { lat: 50.080611, lng: -5.313306 },
+  },
+  {
+    name: "Bantham Beach, Devon, England",
+    location: { lat: 50.278250, lng: -3.878833 },
+  },
+  {
+    name: "Woolacombe, Devon, England",
+    location: { lat: 51.163278, lng: -4.214694 },
+  },
+  {
+    name: "Croyde Beach, Devon, England",
+    location: { lat: 51.132639, lng: -4.238444 },
+  },
+  {
+    name: "Saltburn, North Yorkshire, England",
+    location: { lat: 54.586222, lng: -0.969222 },
+  },
+  {
+    name: "Kimmeridge Bay, Dorset, England",
+    location: { lat: 50.611111, lng: -2.130472 },
+  },
+  {
+    name: "Llangennith, Gower Peninsula, Wales",
+    location: { lat: 51.592083, lng: -4.294056 },
+  },
+  {
+    name: "Newgale, Pembrokeshire, Wales",
+    location: { lat: 51.851889, lng: -5.125278 },
+  },
+  {
+    name: "Porth Neigwl, Gwynedd, Wales",
+    location: { lat: 52.807667, lng: -4.549694 },
+  },
+  {
+    name: "Rest Bay, Bridgend County, Wales",
+    location: { lat: 51.488750, lng: -3.725361 },
+  },
+  {
+    name: "Thurso East, Scotland",
+    location: { lat: 58.596806, lng: -3.522972 },
+  },
+  {
+    name: "Dalmore Bay, Isle of Lewis, Scotland",
+    location: { lat: 58.305722, lng: -6.757472 },
+  },
+  {
+    name: "Portrush East Strand, County Antrim, Northern Ireland",
+    location: { lat: 55.205694, lng: -6.647694 },
+  },
+  {
+    name: "Lahinch, County Clare, Ireland",
+    location: { lat: 52.933444, lng: -9.348028 },
+  }
+];
 
-  // { lat: 4.56298, lng: 86.35182 },
-  // { lat: 15.77314, lng: -32.27885 },
-  // { lat: -25.77536, lng: -13.37909 },
-  // { lat: 27.22509, lng: 37.68827 },
-]
+
+
+// array of surf spot coordinates
+let surfSpotCoords = []
+for (let i = 0; i < surfSpots.length; i++) {
+  surfSpotCoords.push(surfSpots[i].location)
+}
+
+
 //Determine browser location
 function getPosition() {
 
@@ -82,9 +146,9 @@ function returnDistancesAndDurations(response) {
   return distancesAndDurations
 }
 
+
 //adds and orders distances to surf spots into an array
 function returnSortedDistanceArray(response) {
-
   let distancesAndDurations = returnDistancesAndDurations(response);
   let sortedDistances = [];
   for (let i = 0; i < distancesAndDurations.length; i++) {
@@ -107,9 +171,15 @@ function returnSortedDistanceArray(response) {
   return sortedDistances
 }
 
+//returns sorted place names
+function placeNameArray(response) {
+  console.log('hi');
+}
+
 
 //lists distances to surf spots onto page
 function listDistances(response) {
+  let addresses = returnAddressesArray(response);
   let sortedDistances = returnSortedDistanceArray(response);
   for (let i = 0; i < sortedDistances.length; i++) {
     loc.innerHTML += `<p>${sortedDistances[i]} </p>`;
@@ -123,14 +193,13 @@ async function calcDistance() {
   const service = new google.maps.DistanceMatrixService();
   const request = {
     origins: [pos],
-    destinations: surfSpots,
+    destinations: surfSpotCoords,
     travelMode: google.maps.TravelMode.DRIVING,
     unitSystem: google.maps.UnitSystem.METRIC,
     avoidHighways: false,
     avoidTolls: false,
   }
   service.getDistanceMatrix(request).then((response) => {
-    // console.log(response);
     listDistances(response);
   });
 }
